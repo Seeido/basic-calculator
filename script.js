4658fb6447c5e2c9a3c5afca966ad428654268d8
Seeido <101347929+Seeido@users.noreply.github.com>
2024-06-05 20:24:19 +0300

    Rewrite main calculation functions
    Rewrite and simplify calculation functions to better suit the calculator
    and allow for passing arithmetic operators (as strings) as arguments.

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-let currentValue = undefined; //store current value displayed on calculator
+//Object: define arithmetic operations as methods
+const operators = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => (b !== 0 ? a / b : "Can't divide by zero"), //handle division by zero
+};
 
-//Function: main 'operate' function for calculator
-//desc: takes operation and values/arguments and return value returned by performing chosen operation on given arguments
-function operate(operation, ...nums) {
-  return operation(...nums);
-}
-
-//Functions: basic arithmetic functions for calculator
-function add(...nums) {
-  return nums.reduce((total, i) => {
-    return (total += i);
-  }, 0);
-}
-
-function subtract(...nums) {
-  return nums.reduce((total, i) => {
-    return (total -= i);
-  }, nums[0] * 2);
-}
-
-function multiply(...nums) {
-  return nums.reduce((total, i) => {
-    return (total *= i);
-  }, 1);
-}
-
-function divide(...nums) {
-  return nums.reduce((total, i) => {
-    return (total /= i);
-  }, nums[0] * nums[0]);
+//Function: perform calculation based on given arithmetic operator
+function calculate(a, b, operator) {
+  if (operator in operators) {
+    return operators[operator](a, b);
+  } else {
+    return "Error: Invalid operator";
+  }
 }
